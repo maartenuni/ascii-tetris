5692df84669b2cfdd54d876efd8dc2ea4031ea39
maartenuni <maartenuni@hotmail.com>
2024-01-29 23:05:07 +0100

    Try to make fixing config dir more robust

--- config.py
+++ config.py
@@ -1,57 +1,65 @@
+import logging
 import json
 import os
+import sys
 from pathlib import Path
+
+import pdb
 
 _XDG_CONFIG_HOME = "XDG_CONFIG_HOME"
 _APPDATA = "APPDATA"
 _HOME = "HOME"
 _DIRNAME = "ascii-tetris"
-
-
-def _create_file_from_env_var(envar: str, dirname=_DIRNAME) -> str:
-    """This creates the filename, but has an important side effect
-    that if the config folder doesn't exits it is created, its an error
-    when the directory exists but ain't no folder."""
-    conf_name = Path(os.environ[envar])
-    dirname = conf_name / "ascii-tetris"
-    filename = "ascii-tetris.json"
-    if not dirname.exists():
-        os.makedirs(dirname)
-    if not dirname.is_dir():
-        raise RuntimeError(f"'{dirname}': exists but is not a directory")
-    final_name = str(dirname / filename)
-    return final_name
-
+_CONF_FILE_NAME = "ascii-tetris.json"
 
 class ConfigFile:
     """A class that represents a configuration file"""
+
+    def __init__(self):
+        self.filename =self._get_filename()
+        dirname = os.path.dirname(self.filename)
+        if os.path.exists(dirname):
+            if not os.path.isdir(dirname):
+                raise RuntimeError(f"The dirname'{dirname}' exists but ain't no dir")
+        else:
+            os.makedirs(dirname)
+
 
     def read(self) -> dict:
         """Read the config file, might return empty dict
         when it wasn't present before."""
         ret = {}
+        filename = self._get_filename()
         try:
-            with open(self._get_filename(), 'r') as infile:
+            logging.info(f"Trying to open {filename} for reading")
+            with open(filename, 'r') as infile:
                 ret = json.load(infile)
-        finally:
-            return ret
+        except IOError as error:
+            logging.error(str(error))
+        return ret
 
     def write(self, config:dict)->None:
         """Read the config file, might return empty dict
         when it wasn't present before."""
-        with open(self._get_filename(), 'w') as outfile:
+        filename = self._get_filename()
+        logging.info(f"Trying to open {filename} for reading")
+        with open(filename, 'w') as outfile:
             json.dump(config, outfile, indent=4)
 
-
     @staticmethod
-    def _get_filename():
+    def _get_filename()->str:
         """Get and/or create the config file name"""
         filename = None
         if _XDG_CONFIG_HOME in os.environ:
-            filename = _create_file_from_env_var(_XDG_CONFIG_HOME)
+            filename = Path(os.environ[_XDG_CONFIG_HOME])
+            filename = filename / _DIRNAME / _CONF_FILE_NAME
+        elif _HOME in os.environ:
+            filename = Path(os.environ[_HOME])
+            filename = filename / ".config/" / _DIRNAME / _CONF_FILE_NAME
         elif _APPDATA in os.environ:
-            filename = _create_file_from_env_var(_APPDATA)
+            filename = Path(os.environ[_APPDATA])
+            filename = filename / _DIRNAME / _CONF_FILE_NAME
         else:
-            filename = _create_file_from_env_var(_HOME, "." + _DIRNAME)
+            raise RuntimeError("No suitable folder for config data found.")
 
-        return filename
+        return str(filename)
