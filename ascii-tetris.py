d57b135fa3b33b09bd9c009d83fbd4a19e295c8e
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2024-02-05 22:03:57 +0100

    Detect whether terminal supports color
    fixes #11

--- ascii-tetris.py
+++ ascii-tetris.py
@@ -86,14 +86,14 @@ def _game_loop(args, tgame: Tetris, stdscr, win, next_win=None, score_win=None) 
 
         now = time.time()
         if now - running_time_inc > inc_timeout:  # make the tetrominoe fall
-            logging.debug(f"now = {now}, inc_timeout = {inc_timeout}") 
+            logging.debug(f"now = {now}, inc_timeout = {inc_timeout}")
             tgame.increment()
             running_time_inc += inc_timeout
             did_something = True
 
         if now - running_time_speed > speed_up_timeout:  # speed up the game
-            logging.info(f"now = {now}, speed_up_timeout = {speed_up_timeout}") 
-            inc_timeout = max(0.2, inc_timeout * .95)
+            logging.info(f"now = {now}, speed_up_timeout = {speed_up_timeout}")
+            inc_timeout = max(0.2, inc_timeout * 0.95)
             temp_level += 1
             running_time_speed += speed_up_timeout
 
@@ -101,7 +101,7 @@ def _game_loop(args, tgame: Tetris, stdscr, win, next_win=None, score_win=None) 
 
         if did_something:  # only draw at change of state
             win.clear()  ## clear screen
-            if args.color:
+            if not args.black_and_white:
                 _draw_in_color(win, tgame)
             else:
                 win.addstr(str(tgame))
@@ -111,7 +111,7 @@ def _game_loop(args, tgame: Tetris, stdscr, win, next_win=None, score_win=None) 
         if next_win and tgame.next != next_tile:
             next_tile = copy.deepcopy(tgame.next)
             next_win.clear()
-            if args.color:
+            if not args.black_and_white:
                 _draw_in_color(next_win, next_tile)
             else:
                 next_win.addstr(str(next_tile))
@@ -145,31 +145,31 @@ def _curses_main(stdscr, args) -> int:
 
     stdscr.nodelay(True)
 
-    if args.color:
-        if curses.has_colors():  # init global color pairs
-            curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-            COLOR_PAIRS[LINE.color] = curses.color_pair(1) | curses.A_BOLD
+    if curses.has_colors():  # init global color pairs
+        logging.info("Running with colors")
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        COLOR_PAIRS[LINE.color] = curses.color_pair(1) | curses.A_BOLD
 
-            curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
-            COLOR_PAIRS[MEL.color] = curses.color_pair(2) | curses.A_BOLD
+        curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        COLOR_PAIRS[MEL.color] = curses.color_pair(2) | curses.A_BOLD
 
-            curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-            COLOR_PAIRS[EL.color] = curses.color_pair(3) | curses.A_BOLD
+        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        COLOR_PAIRS[EL.color] = curses.color_pair(3) | curses.A_BOLD
 
-            curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-            COLOR_PAIRS[CUBE.color] = curses.color_pair(4) | curses.A_BOLD
+        curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        COLOR_PAIRS[CUBE.color] = curses.color_pair(4) | curses.A_BOLD
 
-            curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
-            COLOR_PAIRS[ES.color] = curses.color_pair(5) | curses.A_BOLD
+        curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        COLOR_PAIRS[ES.color] = curses.color_pair(5) | curses.A_BOLD
 
-            curses.init_pair(6, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-            COLOR_PAIRS[TABLE.color] = curses.color_pair(6) | curses.A_BOLD
+        curses.init_pair(6, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+        COLOR_PAIRS[TABLE.color] = curses.color_pair(6) | curses.A_BOLD
 
-            curses.init_pair(7, curses.COLOR_RED, curses.COLOR_BLACK)
-            COLOR_PAIRS[MES.color] = curses.color_pair(7) | curses.A_BOLD
-        else:
-            print("Running without colors", file=sys.stderr)
-            args.color = False
+        curses.init_pair(7, curses.COLOR_RED, curses.COLOR_BLACK)
+        COLOR_PAIRS[MES.color] = curses.color_pair(7) | curses.A_BOLD
+    else:
+        logging.info("Running without colors")
+        args.black_and_white = True
 
     board_win = curses.newwin(Tetris.str_height() + 1, Tetris.str_width() + 1)
     next_win = curses.newwin(8, 8, 2, Tetris.str_width() + 4)
@@ -189,15 +189,26 @@ def main():
         "error": logging.ERROR,
     }
     cmdparser = ap.ArgumentParser("Tetris", "Play Tetᴙis in ASCII style", "Enjoy!!")
-    cmdparser.add_argument("-c", "--color", action="store_true", help="Play in color")
-    cmdparser.add_argument("-l", "--log-file", type=str, help="specify a log file to log to.")
-    cmdparser.add_argument("-L", "--log-level", type=str, choices=loglevelmap.keys(), default="info", help="specify the desired loglevel.")
+    cmdparser.add_argument(
+        "-b", "--black-and-white", action="store_true", help="Play in color"
+    )
+    cmdparser.add_argument(
+        "-l", "--log-file", type=str, help="specify a log file to log to."
+    )
+    cmdparser.add_argument(
+        "-L",
+        "--log-level",
+        type=str,
+        choices=loglevelmap.keys(),
+        default="info",
+        help="specify the desired loglevel.",
+    )
 
     args = cmdparser.parse_intermixed_args()
 
     if args.log_file:
         level = loglevelmap[args.log_level]
-        logging.basicConfig(filename = args.log_file, level=logging.DEBUG)
+        logging.basicConfig(filename=args.log_file, level=logging.DEBUG)
         logging.info("logging with loglevel: {}".format(args.log_level.upper()))
 
     try:
